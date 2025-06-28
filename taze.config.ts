633e2492827050d9a9aec0900e1040ece783e9de
a949066041 <wangsh0508@gmail.com>
2025-06-28 21:28:33 +0800

    chore: update dep

---
+++ taze.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'taze'
+
+export default defineConfig({
+  // ignore packages from bumping
+  exclude: [
+    'webpack',
+    'expo',
+  ],
+  force: true,
+  write: true,
+  install: true,
+  ignorePaths: [
+    '**/node_modules/**',
+    '**/test/**',
+  ],
+  // ignore package.json that in other workspaces (with their own .git,pnpm-workspace.yaml,etc.)
+  ignoreOtherWorkspaces: true,
+  // override with different bumping mode for each package
+  packageMode: {
+  },
+  // disable checking for "overrides" package.json field
+  depFields: {
+  },
+})
